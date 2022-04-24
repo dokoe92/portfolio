4c654c9f2d7c6875459194d1dd39a7bfe1557104
Dominik <dokoe92@gmail.com>
2022-04-24 16:39:28 +0200

    added some link stylings, button styling, and a dropdown menu for mobile

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,4 +13,22 @@ function toggleProfileImg(){
             profilePicture.setAttribute("src", "./resources/images/profile normal.jpg");
         }
     
-}
+};
+
+function dropdown(){
+    document.getElementById("myDropdown").classList.toggle("show");
+};
+
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
